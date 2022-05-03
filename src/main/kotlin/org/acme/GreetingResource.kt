278e746208776541fd0ff08c7595e2e35dd313d5
lczhi <lczhi@ehanlin.com.tw>
2022-05-03 10:07:15 +0800

    clearup

--- src/main/kotlin/org/acme/GreetingResource.kt
+++ src/main/kotlin/org/acme/GreetingResource.kt
@@ -4,38 +4,21 @@ import com.mongodb.MongoException
 import io.quarkus.qute.Template
 import io.quarkus.qute.TemplateInstance
 import org.bson.Document
-import org.jboss.resteasy.reactive.RestQuery
-import java.net.URI
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
-
-
 
 @ApplicationScoped
-@Path("/hello")
-class HelloResource {
-    @Inject
-    lateinit var mongoData: Template
-    @GET()
-    @Produces(MediaType.TEXT_PLAIN)
-    fun printWorld(): TemplateInstance{
-        return mongoData.data("name", "I AM here")
-    }
-}
-
 @Path("UserData")
 class userData(private  val kotlinService: KotlinService) {
 
     @Inject
     lateinit var InsertUser: Template
-
     @GET()
     @Produces(MediaType.TEXT_PLAIN)
-    fun printWorld(): TemplateInstance{
+    fun loginPage(): TemplateInstance{
         return InsertUser.instance()
     }
 
@@ -44,16 +27,15 @@ class userData(private  val kotlinService: KotlinService) {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
 
-    fun UserInsert(body: Map<String, String>){
+    fun userInsert(body: Map<String, String>){
 
-        var Acc: String = body["newAcc"]!!
-        var Pw: String = body["newPw"]!!
+        var acc: String = body["newAcc"]!!
+        var pw: String = body["newPw"]!!
         try {
-            kotlinService.insertUser(Acc, Pw)
+            kotlinService.insertUser(acc, pw)
         } catch (e: MongoException) {
             e.printStackTrace()
         }
-//        return Response.seeOther(URI("https://www.google.com")).build()
     }
 
     @Path("Check")
@@ -72,16 +54,38 @@ class userData(private  val kotlinService: KotlinService) {
             e.printStackTrace()
         }
     }
+}
+
+@Path("/UserProfile")
+class ProfileData(private  val kotlinService: KotlinService) {
+    @Path("ProfileData")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun profileDataSearch(): ArrayList<Document> {
+        return kotlinService.searchProfileData(1)
+    }
+
+    @Path("UpdatePro")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    fun updataProfile(){
+        try {
+            kotlinService.updateProfileData(1, 10000)
+        }catch (e: MongoException){
+            e.printStackTrace()
+        }
+    }
+}
+
+
+
+
 //    @Path("/Search/{_id}")
 //    @Produces(MediaType.TEXT_PLAIN)
 //    @PUT
 //    fun searchID(@PathParam("_id") _id: Int): ArrayList<Document> {
 //        return kotlinService.SUser(_id)
 //    }
-}
-
-
-
 
 //@Path("/UserData")
 //class userData(private  val kotlinService: KotlinService){
@@ -103,26 +107,7 @@ class userData(private  val kotlinService: KotlinService) {
 //    }
 //}
 
-@Path("/UserProfile")
-class ProfileData(private  val kotlinService: KotlinService) {
-    @Path("ProfileData")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    fun ProfileDataSearch(): ArrayList<Document> {
-        return kotlinService.SearchProfileData(1)
-    }
 
-    @Path("UpdatePro")
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    fun UpdataProfile(){
-        try {
-            kotlinService.UpdateProfileData(1, 10000)
-        }catch (e: MongoException){
-            e.printStackTrace()
-        }
-    }
-}
 
 //@Path("warm/up")
 //class GradlePractice{
@@ -150,11 +135,22 @@ class ProfileData(private  val kotlinService: KotlinService) {
 //        return kotlinService.SUserPro()
 //    }
 //}
-
-@Path("/hello2")
-class HelloTest {
-    @GET()
-    @Produces(MediaType.TEXT_PLAIN)
-    fun printWorld() = "My first quarkus application."
-}
+//
+//@Path("/hello2")
+//class HelloTest {
+//    @GET()
+//    @Produces(MediaType.TEXT_PLAIN)
+//    fun printWorld() = "My first quarkus application."
+//}
+//
+//@Path("/hello")
+//class HelloResource {
+//    @Inject
+//    lateinit var mongoData: Template
+//    @GET()
+//    @Produces(MediaType.TEXT_PLAIN)
+//    fun printWorld(): TemplateInstance{
+//        return mongoData.data("name", "I AM here")
+//    }
+//}
 
--- src/main/kotlin/org/acme/KotlinService.kt
+++ src/main/kotlin/org/acme/KotlinService.kt
@@ -30,19 +30,41 @@ class KotlinService(private val mongoClient: MongoClient) {
         return getCollection.find(filiter).firstOrNull() != null
     }
 
-    fun findAll(): Int {
-        var findIterable: FindIterable<Document> = getCollection.find().sort(BasicDBObject("_id", -1)).limit(1)
-        var mongoCursor: MongoCursor<Document> = findIterable.iterator()
-        var idNum: Int = mongoCursor.next().get("_id") as Int
-        idNum += 1
-
-        var newuserData: Document = Document()
-            .append("_id", idNum)
-            .append("Account", "Acc")
-            .append("Password", "Pw")
-        getCollection.insertOne(newuserData)
-        return idNum
+    fun searchProfileData(_id: Int): ArrayList<Document> {
+        fun findIterable(query: BasicDBObject): FindIterable<Document> = getCollection.find(query)
+        var mongocursor: MongoCursor<Document> = findIterable(BasicDBObject("_id", _id)).iterator()
+        var newresult = arrayListOf<Document>()
+        while (mongocursor.hasNext()) {
+            newresult.add(mongocursor.next())
+        }
+        return newresult
     }
+
+    fun updateProfileData(user_id: Int,credit: Int) {
+        val updatefliiter: Document = Document()
+            .append("user_id", user_id)
+        val updatevalue: Document = Document()
+            .append("user_credit", credit)
+        val updateobject :Document = Document()
+        updateobject.append("\$set", updatevalue)
+        getCollectionPro.updateOne(updatefliiter, updateobject)
+    }
+}
+
+
+//fun findAll(): Int {
+//    var findIterable: FindIterable<Document> = getCollection.find().sort(BasicDBObject("_id", -1)).limit(1)
+//    var mongoCursor: MongoCursor<Document> = findIterable.iterator()
+//    var idNum: Int = mongoCursor.next().get("_id") as Int
+//    idNum += 1
+//
+//    var newuserData: Document = Document()
+//        .append("_id", idNum)
+//        .append("Account", "Acc")
+//        .append("Password", "Pw")
+//    getCollection.insertOne(newuserData)
+//    return idNum
+//}
 
 //    fun SUser(_id: Int): ArrayList<Document> {
 //        fun findIterable(query: BasicDBObject): FindIterable<Document> = getCollection.find(query)
@@ -54,39 +76,14 @@ class KotlinService(private val mongoClient: MongoClient) {
 //        return newresult
 //    }
 
-    fun SUserPro(): ArrayList<Document> {
-        fun findIterable(query : BasicDBObject): FindIterable<Document> = getCollectionPro.find(query)
-        var mongoCursor : MongoCursor<Document> = findIterable(BasicDBObject("User_Credit", 2)).iterator()
-        var newresult = arrayListOf<Document>()
-        while (mongoCursor.hasNext()){
-            newresult.add(mongoCursor.next())
-        }
-        return newresult
-    }
-
-
-
-    fun SearchProfileData(_id: Int): ArrayList<Document> {
-        fun findIterable(query: BasicDBObject): FindIterable<Document> = getCollection.find(query)
-        var mongoCursor: MongoCursor<Document> = findIterable(BasicDBObject("_id", _id)).iterator()
-        var newresult = arrayListOf<Document>()
-        while (mongoCursor.hasNext()) {
-            newresult.add(mongoCursor.next())
-        }
-        return newresult
-    }
-
-    fun UpdateProfileData(userId: Int,credit: Int) {
-        val updateFliiter: Document = Document()
-            .append("UserId", userId)
-        val updateValue: Document = Document()
-            .append("User_Credit", credit)
-        val updateObject :Document = Document()
-        updateObject.append("\$set", updateValue)
-        getCollectionPro.updateOne(updateFliiter, updateObject)
-    }
-}
-
-
+//fun SUserPro(): ArrayList<Document> {
+//    fun findIterable(query : BasicDBObject): FindIterable<Document> = getCollectionPro.find(query)
+//    var mongoCursor : MongoCursor<Document> = findIterable(BasicDBObject("User_Credit", 2)).iterator()
+//    var newresult = arrayListOf<Document>()
+//    while (mongoCursor.hasNext()){
+//        newresult.add(mongoCursor.next())
+//    }
+//    return newresult
+//}
 
 
